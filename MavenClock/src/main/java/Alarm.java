b3f513130abd17791ef8076fcc87d63aaca73e11
Alfred79 <34650397+Alfred79@users.noreply.github.com>
2019-01-29 18:17:48 +0100

    Har skapat och testat en Alarmklass

--- MavenClock/src/main/java/Alarm.java
+++ MavenClock/src/main/java/Alarm.java
@@ -1,4 +1,74 @@
 
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.swing.JOptionPane;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 public class Alarm {
-	
+		private boolean isOn;
+	//denna alarmtid ska sättas av användaren
+		private Calendar alarmTime; 
+		//formaterar alarmtiden till enhetlig output som string
+		private DateFormat alarmFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
+		
+		//Konstruerar alarmet och sätter alarmtiden
+				public Alarm(int year, int month, int date, int hour, int minute) {
+				this.setAlarmTime(year, month, date, hour, minute);	
+				}
+		
+		//Objekt för att läsa in och spela upp alarmljudet
+		private InputStream soundFileInputStream;
+		@SuppressWarnings("restriction")
+		private AudioStream audioStream;
+		@SuppressWarnings("restriction")
+		private AudioPlayer alarmSoundPlayer = null;
+		File soundFile = new File("src/main/resources/Soundfiles/Alien_AlarmDrum-KevanGC-893953959.wav");
+		///MavenClock/src/main/resources/Soundfiles/sms-alert-5-daniel_simon.wav
+		
+		public void triggerAlarm() {
+			
+			System.out.println("triiiiiiigeredddd");
+			new Thread() {
+				public void run() {
+					try {
+						soundFileInputStream = new FileInputStream(soundFile);
+						audioStream = new AudioStream(soundFileInputStream);
+						alarmSoundPlayer.player.start(audioStream);
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (Exception e) {
+						
+					}
+				}
+			}.start();
+			
+		}
+		
+		
+		//getters och setters
+		public boolean isOn() {
+			return isOn;
+		}
+		public void setOn(boolean isOn) {
+			this.isOn = isOn;
+		}
+		public String getAlarmTime() {
+			String alarmTimeString = alarmFormatter.format(alarmTime.getTime());
+			return alarmTimeString;
+		}
+		public void setAlarmTime(int year, int month, int date, int hour, int minute) {
+			alarmTime = new GregorianCalendar(year, month,date,hour,minute,0); 
+		//0=jan, 1=feb , osv
+		} 
 }
--- MavenClock/src/main/java/ClockWindow.java
+++ MavenClock/src/main/java/ClockWindow.java
@@ -1,16 +1,17 @@
 
-
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
 import java.awt.Color;
 import javax.swing.BoxLayout;
 import javax.swing.JTextPane;
 import java.awt.Component;
 import java.awt.Font;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -19,76 +20,69 @@ import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-
 public class ClockWindow extends JFrame {
 	String dateFormatEdit;
 	GregorianCalendar calendarObjekt;
 	int count = 0;
+	private Alarm alarm = new Alarm(2019, 0, 29, 18, 10);
+	private JPanel timeDisplayPanel;
+	private DateFormat formatDateObject = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	
-	private JPanel timeDisplayPanel;@Override
+	@Override
 	public Font getFont() {
 		// TODO Auto-generated method stub
 		return super.getFont();
 	}
 	
-	private JTextPane timeDisplayText; 
+	private JTextPane timeDisplayText;
 	
 	//Konstruera klockfönstret
 	public ClockWindow() {
-	initComponents();
-	displayTime(); 
+		initComponents();
+		displayTime();
 	}
 	
-	/*Startar tråd som läser in ny tid varje sekund
-	 * Adderar sedan tiden till fönstret */
+	/* Startar tråd som läser in ny tid, adderar sedan tiden till fönstret */
 	private void displayTime() {
-		new Thread () {
-			public void run() 
-			{
-			
-			while (true) 
-			{
-				calendarObjekt = new GregorianCalendar();
-				//String dateString = calendarObjekt.getTime().toString();
+		new Thread() {
+			public void run() {
 				
-				if (count == 0) 
-					{
-					dateFormatEdit = formatFMAM(calendarObjekt);
+				while (true) {
+					calendarObjekt = new GregorianCalendar();
+					//currentTime används för att jämföra med den satta alarmtiden
+					String currentTime = formatDateObject.format(calendarObjekt.getTime());
+					compareToAlarmTime(currentTime);
+					
+					if (count == 0) {
+						dateFormatEdit = formatFMAM(calendarObjekt);
 					}
-				count++;
-				
-				
-				if (dateFormatEdit.length()>15) 
-				{
-				dateFormatEdit = formatDate(calendarObjekt);
-				}
-				else if (dateFormatEdit.contains("M")) 
-				{
-				dateFormatEdit = formatFMAM(calendarObjekt);
-				}
-				else {
-					dateFormatEdit = format24h(calendarObjekt);
+					count++;
+					
+					if (dateFormatEdit.length() > 15) {
+						dateFormatEdit = formatDate(calendarObjekt);
+					} else if (dateFormatEdit.contains("M")) {
+						dateFormatEdit = formatFMAM(calendarObjekt);
+					} else {
+						dateFormatEdit = format24h(calendarObjekt);
 					}
-			
-			
-			//String dateFormatEdit = dateString.substring(11, 19);
-			System.out.println(dateFormatEdit);
-			try {
-					this.sleep(500);//pausa 1 sek
-				} catch (InterruptedException e) {
-				e.printStackTrace();
+					
+					//String dateFormatEdit = dateString.substring(11, 19);
+					System.out.println(dateFormatEdit);
+					try {
+						this.sleep(500);//pausa 0.5 sek
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					showTime(dateFormatEdit); //Uppdatera textfältet som visar tiden
 				}
-				showTime(dateFormatEdit); //Uppdatera textfältet som visar tiden
-				}
-			}	
-			}.start();
-		
+			}
+		}.start();
 		
 	}
 	
 	//skapar och sätter inställningar för grafiska objekt till fönstret
 	private void initComponents() {
-		setSize(900,600);
+		setSize(900, 600);
 		timeDisplayPanel = new JPanel();
 		timeDisplayPanel.setBackground(new Color(245, 245, 220));
 		timeDisplayPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -103,7 +97,6 @@ public class ClockWindow extends JFrame {
 				dateFormatEdit = formatFMAM(calendarObjekt);
 			}
 		});
-		
 		
 		timeDisplayPanel.add(btnNewButton_1);
 		
@@ -121,7 +114,6 @@ public class ClockWindow extends JFrame {
 			}
 		});
 		
-		
 		timeDisplayPanel.add(btnNewButton_2);
 		timeDisplayPanel.add(btnNewButton);
 		timeDisplayText = new JTextPane();
@@ -133,39 +125,48 @@ public class ClockWindow extends JFrame {
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
 	}
+	
 	//Lägger till tiden till visningsfönstret
 	public void showTime(String time) {
 		timeDisplayText.setText(time);
-		
-		
-		}
+	}
 	
 	// Formaterar tiden - formatmall kan hittas här:
 	// https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
 	
-	public static String formatFMAM(GregorianCalendar calendar){
-	    SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss a");
-	    fmt.setCalendar(calendar);
-	    String dateFormatted = fmt.format(calendar.getTime());
-	    
-	   // EM/AM till stora bokstäver
-	    return dateFormatted.toUpperCase();
-	}
-	public static String format24h(GregorianCalendar calendar){
-	    SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss");
-	    fmt.setCalendar(calendar);
-	    String dateFormatted = fmt.format(calendar.getTime());
-	    
-	   // EM/AM till stora bokstäver
-	    return dateFormatted;
+	public static String formatFMAM(GregorianCalendar calendar) {
+		SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss a");
+		fmt.setCalendar(calendar);
+		String dateFormatted = fmt.format(calendar.getTime());
+		
+		// EM/AM till stora bokstäver
+		return dateFormatted.toUpperCase();
 	}
 	
-	public static String formatDate(GregorianCalendar calendar){
-	    SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss" + " EEE MMM d yyyy");
-	    fmt.setCalendar(calendar);
-	    String dateFormatted = fmt.format(calendar.getTime());
-	    
-	   // Bokstäver till stora bokstäver
-	    return dateFormatted.toUpperCase();
+	public static String format24h(GregorianCalendar calendar) {
+		SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss");
+		fmt.setCalendar(calendar);
+		String dateFormatted = fmt.format(calendar.getTime());
+		
+		// EM/AM till stora bokstäver
+		return dateFormatted;
 	}
+	
+	public static String formatDate(GregorianCalendar calendar) {
+		SimpleDateFormat fmt = new SimpleDateFormat("hh:mm:ss" + " EEE MMM d yyyy");
+		fmt.setCalendar(calendar);
+		String dateFormatted = fmt.format(calendar.getTime());
+		
+		// Bokstäver till stora bokstäver
+		return dateFormatted.toUpperCase();
+	}
+	
+	//jämför om den nuvarande tiden är samma som alarmet
+	private void compareToAlarmTime(String currentTime) {
+		if (currentTime.equals(alarm.getAlarmTime())) {
+			//utlöser alarm om det är samma tid
+			alarm.triggerAlarm();
+		}
+	}
+	
 }
